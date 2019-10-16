5b845204142fee1d50bd8fc3cf719c5e86ed6edd
jemimagregory <jgregory@storans.school.nz>
2019-10-16 20:36:17 +1300

    Displaying the pet's current weight to the user.

---
+++ check_pet_weight.py
@@ -0,0 +1,13 @@
+def check_pet_weight(pet_weight, min_weight, max_weight):
+
+    # Displays the maximum and minimum weights the pet has to between
+    print("Your pet has to be between {}kg and {}kg to stay alive and happy.".format(min_weight, max_weight))
+
+    # Displays the pets current weight for the user.
+    print("Currently your pet weighs {}kg, so your pet is alive and happy!".format(pet_weight))
+
+
+# Main Routine
+
+# calling the function to test with general inputs
+check_pet_weight(2.3, 1.5, 2.5)
