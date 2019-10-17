a7f62342705ffa2da79af5c69a1a584c09bc1fb2
jemimagregory <jgregory@storans.school.nz>
2019-10-18 10:45:53 +1300

    Introduces the user to their pet and displays a fun image of it.

---
+++ welcome_screen.py
@@ -0,0 +1,20 @@
+# This function is used after the main explanation of how the program works
+def welcome_screen(pet_name):
+    # displays an introduction of the pet rabbit to the user
+    print("This is your pet, {}!".format(pet_name))
+    print("   _________         ________ \n"
+          "  /         \       /         \  \n"
+          " /  /~~~~~\  \     /  /~~~~~\  \ \n"
+          " |  |     |  |     |  |     |  | \n"
+          " |  |     |  |     |  |     |  | \n"
+          " |  |     |  |     |  |     |  |         / \n"
+          " |  |     |  |     |  |     |  |       // \n"
+          "(o  o)    \  \_____/  /     \  \_____/ / \n"
+          " \__/      \         /       \        / \n"
+          "  |         ~~~~~~~~~         ~~~~~~~~ \n"
+          "  ^")
+
+
+# Main Routine
+
+welcome_screen("Rodney")
