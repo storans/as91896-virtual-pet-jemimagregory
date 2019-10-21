c93fbfcdb85b9b412432beff4ce80db2dc3df5de
jemimagregory <jgregory@storans.school.nz>
2019-10-21 15:53:27 +1300

    assigns what type of death the pet had -over or underweight. -with testing aspects at the end

--- check_if_dead.py
+++ check_if_dead.py
@@ -6,7 +6,7 @@ def check_if_dead(pet_weight, min, max):
         death_end()
 
         # if the pet_weight exceeds the maximum weight, assigns death to 'overweight'
-        death = "overweight"
+        death_type = "overweight"
 
         # returns the death so it can be used outside the function (in the death_screen function)
         return death
@@ -17,15 +17,33 @@ def check_if_dead(pet_weight, min, max):
         death_end()
 
         # if the pet_weight doesn't reach the minimum weight, assigns death to 'underweight'
-        death = "underweight"
+        death_type = "underweight"
 
         # returns the death so it can be used outside the function (in the death_screen function)
-        return death
+        return death_type
 
     # if the pet is still within the minimum and maximum weights the function ends
     else:
         return
 
+
 # death_end function for testing purposes
 def death_end():
     print("death end function, will eventually be in this place")
+
+
+# Main Routine
+
+# setting pet_weight for testing
+pet_weight = float(input("enter the pet's weight: "))
+
+# setting minimum_weight for testing
+minimum_weight = 1.5
+
+# setting maximum_weight for testing
+maximum_weight = 2.5
+
+death = check_if_dead(pet_weight, minimum_weight, maximum_weight)
+
+# print 'death' for testing if it can now be used outside of the function and that the function gets the output i expect
+print(death)
